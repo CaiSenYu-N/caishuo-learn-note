8a66bb426a2c882024368a620f21293bf78324fb
CaiSenYu-N <392818050@qq.com>
2018-10-25 19:43:40 +0800

    1025add

--- work/20181025/年终奖计算/js/main.js
+++ work/20181025/年终奖计算/js/main.js
@@ -7,19 +7,19 @@ if(year<1){
     rs = vage;
   }else{
     rs = vage*1.2;
-  }
+  };
 }else if(year>=1 && year<2){
   if(vage<=10000){
     rs = vage*1.5;
   }else{
     rs = vage *1.7;
-  }
+  };
 }else if(year>=2){
   if(vage<=12000){
     rs = vage*3;
   }else{
     rs = vage*3.2;
-  }
+  };
 }
 
 alert('您的工作年限为：'+year+'年;'+'您的年终奖为：'+rs+'元;');
