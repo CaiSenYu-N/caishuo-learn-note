f43edc93d7b4873a122b193542f2355eb5683c22
CaiSenYu-N <392818050@qq.com>
2018-10-28 20:52:44 +0800

    add

---
+++ work/20181025/2.1题/js/main.js
@@ -0,0 +1,14 @@
+function isEnd(7) {
+  var len = (n+"len").length;
+  if(len==1) {
+    if(len==7){
+      return false;
+    }else{
+      return true;
+    }
+  }else if{
+    if(len>1){
+
+    }
+  }
+}
