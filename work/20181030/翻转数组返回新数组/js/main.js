8221023b4bfc9866b6949fe61d5041661d526d43
CaiSenYu-N <392818050@qq.com>
2018-11-01 10:59:23 +0800

    add

--- work/20181030/翻转数组返回新数组/js/main.js
+++ work/20181030/翻转数组返回新数组/js/main.js
@@ -10,3 +10,54 @@ function reverse(arr){
 
 reverse(arr);
 console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// var arr = ['a','b','c','d','e'];
+//
+// function reverse(arr){
+//   for(var i=0; i<Math.floor(arr.length/2); i++){
+//     var temp = arr[i];
+//     var arr[i] = arr[arr.length-1-i];
+//     var arr[arr.length-1-i] = temp;
+//   }
+// }
+//
+// reserve(arr);
+// console.log(arr);
+
+
+
+
+
+
+var arr = ['a','b','c','d','e'];
+
+function resever(arr){
+  for(var i=0; i<Math.floor(arr.length/2); i++){
+    var temp = arr[i];
+    arr[i] = arr(arr.length-1-i);
+    arr[arr.length-1-i] = temp;
+  }
+}
+
+resever(arr);
+console.log(arr);
--- work/20181031/求阶乘的和/js/main.js
+++ work/20181031/求阶乘的和/js/main.js
@@ -1,4 +1,4 @@
-// var n = prompt('请输入数字：')；
+  // var n = prompt('请输入数字：')；
 // console.log('您输入的数字为'+n+'该数字的阶乘为'+total(n));
 //
 // function total(num){
@@ -16,8 +16,27 @@
 //   return n*jc(n-1);
 // }
 
-var n = prompt('请输入数字：');
-console.log('您输入的数字为'+n+'该数字的阶乘为'+total(n));
+// var n = prompt('请输入数字：');
+// console.log('您输入的数字为'+n+'该数字的阶乘为'+total(n));
+//
+// function total(num){
+//   var sum = 0;
+//   for(var i=1; i<=num; i++){
+//     sum += jc(i);
+//   }
+//   return sum;
+// }
+//
+// function jc(n){
+//   if(n==1){
+//     return 1;
+//   }
+//   return n*jc(n-1);
+// }
+
+
+var n = prompt('请输入：');
+console.log('您输入的数字为：'+ n + '该数字的阶乘为' + total(n));
 
 function total(num){
   var sum = 0;
